7999a2e8a4d529ea3e2fede315ada782c96c2c3c
Quinten <q.deputter@tilburguniversity.edu>
2021-10-03 21:55:07 +0200

    Getting data into R, cleaning from here on

--- Data/data_cleaning.R
+++ Data/data_cleaning.R
@@ -1,9 +1,23 @@
 # --- Load Libraries --- #
 
 library(dplyr)
-
+getwd()
 # --- Cleaning data --- #
 
-Curfew<- rbind(january_2021,february_2021,march_2021, april_2021, may_2021)
+#Getting data in R, 
 
-Curfew_cleaned <- Curfew %>% select('id','last_scraped', 'price', 'host_is_superhost', 'neighbourhood_cleansed')
+Aug2020 <- read_csv("Data/period_aug2020.csv")
+sep2020 <- read_csv("Data/period_sep2020.csv")
+oct2020 <- read_csv("Data/period_oct2020.csv")
+nov2020 <- read_csv("Data/period_nov2020.csv")
+dec2020 <- read_csv("Data/period_dec2020.csv")
+jan2021 <- read_csv("Data/period_jan2021.csv")
+feb2021 <- read_csv("Data/period_feb2021.csv")
+mar2021 <- read_csv("Data/period_mar2021.csv")
+apr2021 <- read_csv("Data/period_apr2021.csv")
+may2021 <- read_csv("Data/period_may2021.csv")
+jun2021 <- read_csv("Data/period_jun2021.csv")
+jul2021 <- read_csv("Data/period_jul2021.csv")
+aug2021 <- read_csv("Data/period_aug2021.csv")
+
+
