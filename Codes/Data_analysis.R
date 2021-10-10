b915b147fdb92d1b7dbaa4b7be8b282981c36bc4
Quinten <q.deputter@tilburguniversity.edu>
2021-10-10 15:03:55 +0200

    Splitted analysis and figure creation

--- Codes/Data_analysis.R
+++ Codes/Data_analysis.R
@@ -1,12 +1,5 @@
 #regression and analysis ideas price is dependent variable 
 
-# curfew 21:00 vs 22:00 
-# effect neighbourhoods on the airbnb prices
-# effect of being super host on the price
-# complete model analysis 
-# plot to see price development as general first plot / historgram also possible here
-# anova testing for neighbourhoods, curfew/ 
-
 # packages
 library(ggplot2)
 library(ggfortify)
@@ -18,44 +11,6 @@ library(modelsummary)
 
 # --- loading curfew data ---#
 Curfew_Amsterdam <- read_csv("./data/Curfew_Amsterdam.csv")
-Curfew_Amsterdam <- read_csv("./data/datacompl.csv") # take this one out when github has been updated 
-
-
-# --- Draft graphs --- #
-
-#Development of the mean price of a room in Amsterdam over the 13 month period
-#grouping data together per month
-Curfew_Amsterdam_plots <- Curfew_Amsterdam
-Curfew_Amsterdam_plots$date <- format(Curfew_Amsterdam_plots$date, "%Y/%m")
-
-Curfew_Amsterdam_plots %>% 
-  ggplot(aes(x = date, y = price, group = "date")) + 
-  geom_smooth(color = 'black', se = FALSE) +
-  labs(x = "Date in months", y = "Price ($)",
-  title = "Figure 1: Airbnb Listing Prices in Amsterdam",
-  subtitle = "From August 2020 till August 2021") +
-  theme_bw()
-  #how to adjust 7_scale without messing up graph
-#! graph needs to be adjusted on the y axis, change x axis dates to month written --> Aug 2020, Sep 2020
-
-#Development of the mean price of a room in Amsterdam superhost or not
-
-Curfew_Amsterdam_plots %>% 
-  ggplot(aes(x = date, y = price, group = host_is_superhost, color = host_is_superhost)) +
-  geom_smooth(se = FALSE) +
-  labs(x = "Date", y = "Price ($)",
-       title = "Figure 2: Airbnb Listing Prices for Super Host and Normal host status",
-       subtitle = "From August 2020 till August 2021",) +
-  theme_dark() +
-  scale_color_manual(values = c("green", "orange")) #change title here but ran into an error message 
-
-
-#Development of the price in different neighbourhoods --> one graph preferred 
-
-Curfew_Amsterdam_plots %>% 
-  ggplot(aes(x = date, y = price, group = neighbourhood, color = neighbourhood)) + #not sure about this graph in particular, looks messy
-  geom_smooth(se = FALSE) 
-
 
 # --- Linear Regression models --- #
 
