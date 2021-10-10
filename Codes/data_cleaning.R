1962f9e4fa82f9fb1889befffcdf5d5715f074fa
Quinten <q.deputter@tilburguniversity.edu>
2021-10-10 15:13:38 +0200

    Splitting dummy creation and cleaning doc

--- Codes/data_cleaning.R
+++ Codes/data_cleaning.R
@@ -4,7 +4,6 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(data.table)
-library(stargazer)
 library(modelsummary)
 
 # --- Load data in R Environment --- #
@@ -44,23 +43,7 @@ datacompl$neighbourhood <- as.factor(datacompl$neighbourhood)
 # --- Checking duplicates & checking summary --- #
 
 datacompl <- datacompl %>% filter(!duplicated(datacompl))
-
 summary(datacompl)
 
-######### cleaning finished, make dummies and save as csv file#########
-
-
-# --- Create dummy variable for curfew (1 = curfew 0 = no curfew) --- #
-datacompl$curfew <- ifelse( datacompl$date > as.Date("2021/01/23", format = "%Y/%m/%d") &
-                              datacompl$date < as.Date("2021/04/28", format = "%Y/%m/%d"), 1, 0)
-
-# --- Create dummy variable for type of curfew (from 21:00 or from 22:00) --- #
-datacompl$curfew_2100 <- ifelse( datacompl$date > as.Date("2021/01/23", format = "%Y/%m/%d") &
-                                datacompl$date < as.Date("2021/03/31", format = "%Y/%m/%d"), 1, 0)   #original curfew from 21:00 till 04:30
-
-datacompl$curfew_2200 <- ifelse( datacompl$date > as.Date("2021/03/31", format = "%Y/%m/%d") &
-                                datacompl$date < as.Date("2021/04/28", format = "%Y/%m/%d"), 1, 0)   #shorter curfew from 22:00 till 04:30
-
-Curfew_Amsterdam <- datacompl
-rm(datacompl)
-write.csv(Curfew_Amsterdam, "data/Curfew_Amsterdam.csv", row.names = FALSE)
+######### cleaning finished, save as csv file#########
+write.csv(datacompl, "data/datacompl.csv", row.names = FALSE)
