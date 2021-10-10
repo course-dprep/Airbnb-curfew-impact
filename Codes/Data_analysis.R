7a7d0c2fb853ba233bab641d4546efce5446be64
Quinten <q.deputter@tilburguniversity.edu>
2021-10-10 14:45:18 +0200

    small changes to file + questions for Hannes

--- Codes/Data_analysis.R
+++ Codes/Data_analysis.R
@@ -28,16 +28,14 @@ Curfew_Amsterdam <- read_csv("./data/datacompl.csv") # take this one out when gi
 Curfew_Amsterdam_plots <- Curfew_Amsterdam
 Curfew_Amsterdam_plots$date <- format(Curfew_Amsterdam_plots$date, "%Y/%m")
 
-ggplot(data, aes(date, price, group = "date")) + geom_smooth() #does not work -- remove
-
 Curfew_Amsterdam_plots %>% 
   ggplot(aes(x = date, y = price, group = "date")) + 
   geom_smooth(color = 'black', se = FALSE) +
-  labs(x = "Date in months", y = "Price ($)") +
-  ggtitle("Figure 1: Airbnb Listing Prices in Amsterdam from August 2020 till August 2021") +
-  theme_bw() #how to adjust 7_scale without messing up graph
-
-class(Curfew_Amsterdam$host_is_superhost)
+  labs(x = "Date in months", y = "Price ($)",
+  title = "Figure 1: Airbnb Listing Prices in Amsterdam",
+  subtitle = "From August 2020 till August 2021") +
+  theme_bw()
+  #how to adjust 7_scale without messing up graph
 #! graph needs to be adjusted on the y axis, change x axis dates to month written --> Aug 2020, Sep 2020
 
 #Development of the mean price of a room in Amsterdam superhost or not
@@ -45,9 +43,10 @@ class(Curfew_Amsterdam$host_is_superhost)
 Curfew_Amsterdam_plots %>% 
   ggplot(aes(x = date, y = price, group = host_is_superhost, color = host_is_superhost)) +
   geom_smooth(se = FALSE) +
-  labs(x = "Date", y = "Price ($)") +
-  ggtitle("Figure 2: Airbnbn Listing Prices for Superhost and Normal Hosts") +
-  theme_bw() +
+  labs(x = "Date", y = "Price ($)",
+       title = "Figure 2: Airbnb Listing Prices for Super Host and Normal host status",
+       subtitle = "From August 2020 till August 2021",) +
+  theme_dark() +
   scale_color_manual(values = c("green", "orange")) #change title here but ran into an error message 
 
 
@@ -62,8 +61,9 @@ Curfew_Amsterdam_plots %>%
 
 # --- Checking effect curfew and smaller curfew window --- #
 m1 <- lm(price ~ 1 + curfew + host_is_superhost, data = Curfew_Amsterdam)
-m2 <- lm(price ~ 1 + curfew_2100 + curfew_2200 + host_is_superhost, data = Curfew_Amsterdam)
-m3 <- lm(price ~ 1 + curfew_2100 + curfew_2200 + host_is_superhost + neighbourhood, data = Curfew_Amsterdam)
+m2 <- lm(price ~ 1 + curfew21_00 + curfew22_00 + host_is_superhost, data = Curfew_Amsterdam)
+m3 <- lm(price ~ 1 + curfew21_00 + curfew22_00 + host_is_superhost + neighbourhood, data = Curfew_Amsterdam)
+m4 <- lm(price ~ 1 + curfew + curfew22_00 + host_is_superhost + neighbourhood, data = Curfew_Amsterdam) #this one or m3?
 
 table_m1_m2_m3 <- msummary(list(m1, m2, m3))
 table_m1_m2_m3
@@ -80,7 +80,7 @@ autoplot(m3,which = 1:3,nrow = 1,ncol = 3)
 
 pot_outliers <- m3 %>%
   augment() %>%
-  select(price, curfew_2100, curfew_2200, host_is_superhost, neighbourhood, leverage = .hat, cooks_dist = .cooksd) %>%
+  select(price, curfew21_00, curfew22_00, host_is_superhost, neighbourhood, leverage = .hat, cooks_dist = .cooksd) %>%
   arrange(desc(cooks_dist)) %>%
   head()
 pot_outliers
@@ -92,9 +92,8 @@ stargazer(m1, m2, m3,
           title = "Figure 1: Curfew effect on Airbnb Prices",
           dep.var.caption = "Airbnb Pricing",
           dep.var.labels = "",
-          covariate.labels = c("Curfew", "22:00 Curfew", "Superhost"),
           column.labels = c("Original Curfew", "Later Curfew"),
           notes.label = "Significance levels",
-          type = 'text') 
+          type = 'text') #update after asking Hannes about which model we need to use.
 
-stargazer(m1, m2, m3, type = 'text')  # not sure about model
+stargazer(m1, m2, m3, type = 'text')  
